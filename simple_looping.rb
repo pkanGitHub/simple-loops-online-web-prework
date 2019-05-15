a7fb738d94baff1457c23060f4c0081036ba7b29
Phyllis Kan <pyk9q7@gmail.com>
2019-05-15 03:02:01 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,14 +10,18 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
+  loop do
+    puts phrase
+  end
+
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times = gets.chomp
   number_of_times.times do
     puts phrase
+    number_of_times -= 1
   end
 end
 
